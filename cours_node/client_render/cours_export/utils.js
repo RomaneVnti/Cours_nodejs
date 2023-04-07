c1ac11f54b7181f20a7f505909f1055522dd09bc
Romane Valenti <102488081+RomaneVnti@users.noreply.github.com>
2023-04-07 10:01:40 +0200

    Add files via upload

---
+++ cours_node/client_render/cours_export/utils.js
@@ -0,0 +1,18 @@
+// exports.name = "Johns";
+
+// exports.Product = function(name, price) {
+//     this.name = name;
+//     this.price = price;
+// };
+
+
+// On l'utilise pour exporter plusieurs choses 
+exports.Utils = {
+    product : function (name,price ){},
+    count : 0,
+    model : {
+        name : null,
+        email : null,
+    }
+}
+
